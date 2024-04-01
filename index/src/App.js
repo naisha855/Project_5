1092a178c04cee2456e85661cb8a850d23b6d011
naisha855 <156628341+naisha855@users.noreply.github.com>
2024-04-01 17:38:07 +0900

    2024-04-01_01

---
+++ index/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { Container, Navbar, Nav, Row, Col } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+<div className="slider"></div>
+
+    </div>
+  );
+}
+
+export default App;
